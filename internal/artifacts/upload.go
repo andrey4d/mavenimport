fa9bc51578caed2a6135e39cf2c1f7dc3f52e002
andrey4d <andrey4d.dev@gmail.com>
2024-12-07 21:54:03 +0700

    add upload

--- internal/artifacts/upload.go
+++ internal/artifacts/upload.go
@@ -4,21 +4,59 @@
  */
 package artifacts
 
-import "net/http"
+import (
+	"fmt"
+	"io"
 
-func Upload(a Artifact) error {
-	client := http.Client{}
-	_ = client
-	return nil
+	"net/http"
+	"net/url"
+	"strings"
+
+	"log/slog"
+)
+
+type Client struct {
+	BaseURL    string
+	Token      string
+	HTTPClient *http.Client
 }
 
-/*
-curl -v -u admin:admin123
--F "maven2.generate-pom=false"
--F "maven2.asset1=@/absolute/path/to/the/local/file/pom.xml"
--F "maven2.asset1.extension=pom"
--F "maven2.asset2=@/absolute/path/to/the/local/file/product-1.0.0.jar;type=application/java-archive"
--F "maven2.asset2.extension=jar"
-"http://localhost:8081/service/rest/v1/components?repository=maven-releases"
+func NewClient(url, token string) *Client {
 
-*/
+	return &Client{
+		BaseURL:    url,
+		Token:      token,
+		HTTPClient: &http.Client{},
+	}
+}
+
+func (c *Client) Upload(a Artifact) error {
+
+	form := url.Values{}
+	form.Add("maven2.generate", "false")
+	form.Add("maven2.asset1", fmt.Sprintf("@%s/%s", a.Path, a.Pom))
+	form.Add("maven2.asset1.extension", "pom")
+	form.Add("maven2.asset2", fmt.Sprintf("@$s/$s;type=application/java-archive", a.Path, a.Package))
+	form.Add("maven2.asset2.extension", "jar")
+
+	req, err := http.NewRequest("POST", c.BaseURL, strings.NewReader(form.Encode()))
+	if err != nil {
+		slog.Error("error", slog.Any("error", err))
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", "Basic "+c.Token)
+
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		slog.Error("error", slog.Any("error", err))
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		slog.Error("error", slog.Any("error", err))
+	}
+
+	fmt.Println(string(body))
+	return nil
+}
